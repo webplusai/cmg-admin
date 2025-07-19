46d4a32c968501791247a5111b328a272d133a67
webplusai <smupyknight@outlook.com>
2025-07-19 09:37:30 +0200

    Configure API request

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: parseInt(process.env.VITE_PORT || 5173),
+    proxy: {
+      '/api': {
+        target: 'http://185.92.220.208:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+      },
+    },
   },
 })
